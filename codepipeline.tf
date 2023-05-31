6580126c1ceb58dc736e9ef40d5241594f04d30e
Shao Wei <33794625+choyshaowei@users.noreply.github.com>
2023-05-31 18:32:02 +0800

    chore: refactor and fix deprecation

--- codepipeline.tf
+++ codepipeline.tf
@@ -1,152 +1,50 @@
-variable "AWS_REGION" {
-  description = "aws-region"
-  default     = "ap-southeast-1"
-  type        = string
-}
-variable "AWS_ACCOUNT_ID" {
-  description = "aws-account-id"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-variable "AWS_ACCESS_KEY_ID" {
-  description = "aws-key"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "aws-secret"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-variable "GITHUB_TOKEN" {
-  description = "github-token"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-variable "env_vars" {
-  default = {
-    AWS_REGION     = "ap-southeast-1"
-    AWS_ACCOUNT_ID = "640940679593"
-    PROJECT        = "foododo-landing"
-  }
-}
-
-provider "aws" {
-  region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Create an internet gateway and attach it to the VPC
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-# Create a public subnet
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-}
-
-# Create a private subnet
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-}
-
-# Create a NAT gateway for the private subnet
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
-}
-
-# Setup route tables
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "private"
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
-
 # CodePipeline
-resource "aws_s3_bucket" "bucket" {
-  bucket        = "foododo-landing"
-  acl           = "private" // or any other default acl
+resource "aws_s3_bucket" "foododo" {
+  bucket        = "foododo"
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "foododo" {
+  bucket = aws_s3_bucket.foododo.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.foododo]
+  bucket     = aws_s3_bucket.foododo.id
+  acl        = "private"
+}
+
+resource "aws_codestarconnections_connection" "example" {
+  name          = "personal-github"
+  provider_type = "GitHub"
+}
+
 resource "aws_codepipeline" "pipeline" {
-  name     = "foododo-landing-pipeline"
+  name     = "foododo-pipeline"
   role_arn = aws_iam_role.pipeline.arn
 
   artifact_store {
-    location = aws_s3_bucket.bucket.bucket
+    location = aws_s3_bucket.foododo.bucket
     type     = "S3"
   }
-
   stage {
     name = "Source"
-
     action {
       name             = "Source"
       category         = "Source"
-      owner            = "ThirdParty"
-      provider         = "GitHub"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
       version          = "1"
       output_artifacts = ["source_output"]
-
       configuration = {
-        Owner      = "choyshaowei"
-        Repo       = "foododo_landing"
-        Branch     = "master"
-        OAuthToken = var.GITHUB_TOKEN
+        ConnectionArn    = aws_codestarconnections_connection.example.arn
+        FullRepositoryId = "choyshaowei/foododo_landing"
+        BranchName       = "master"
       }
     }
   }
-
   stage {
     name = "Build"
 
@@ -159,7 +57,7 @@ resource "aws_codepipeline" "pipeline" {
       output_artifacts = ["build_output"]
       version          = "1"
       configuration = {
-        ProjectName = aws_codebuild_project.foododo_landing.name
+        ProjectName = aws_codebuild_project.foododo.name
       }
     }
   }
@@ -176,16 +74,17 @@ resource "aws_codepipeline" "pipeline" {
       version         = "1"
 
       configuration = {
-        BucketName = aws_s3_bucket.bucket.bucket
+        BucketName = aws_s3_bucket.foododo.bucket
         Extract    = "true"
       }
     }
   }
 }
 
-resource "aws_codebuild_project" "foododo_landing" {
-  name          = "foododo_landing"
-  description   = "foododo_landing"
+# Build and deploy to ECR as a container
+resource "aws_codebuild_project" "foododo" {
+  name          = "foododo"
+  description   = "foododo"
   service_role  = aws_iam_role.codebuild.arn
   build_timeout = "5"
 
@@ -214,7 +113,7 @@ resource "aws_codebuild_project" "foododo_landing" {
 }
 
 resource "aws_iam_role" "pipeline" {
-  name = "your-pipeline-role-name"
+  name = "aws_iam_role_pipeline"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -229,9 +128,8 @@ resource "aws_iam_role" "pipeline" {
     ]
   })
 }
-
 resource "aws_iam_role_policy" "pipeline" {
-  name = "your-pipeline-role-policy-name"
+  name = "aws_iam_role_policy_pipeline"
   role = aws_iam_role.pipeline.id
 
   policy = jsonencode({
@@ -247,12 +145,11 @@ resource "aws_iam_role_policy" "pipeline" {
         Effect   = "Allow"
         Resource = "*"
       },
-      // Add more permissions as per your requirements
     ]
   })
 }
 resource "aws_iam_role" "codebuild" {
-  name = "your-codebuild-role-name"
+  name = "aws_iam_role_codebuild"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -275,9 +172,8 @@ resource "aws_iam_role" "codebuild" {
     ]
   })
 }
-
 resource "aws_iam_role_policy" "codebuild" {
-  name = "your-codebuild-role-policy-name"
+  name = "aws_iam_role_policy_codebuild"
   role = aws_iam_role.codebuild.id
 
   policy = jsonencode({
@@ -288,14 +184,13 @@ resource "aws_iam_role_policy" "codebuild" {
           "s3:GetObject",
           "s3:PutObject",
           "s3:ListBucket",
-          "logs:CreateLogGroup",  // Required for creating log groups
-          "logs:CreateLogStream", // Required for creating log streams
-          "logs:PutLogEvents"     // Required for adding log events
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
         ]
         Effect   = "Allow"
         Resource = "*"
       },
-      // Add more permissions as per your requirements
     ]
   })
 }
@@ -303,19 +198,19 @@ resource "aws_iam_role_policy" "codebuild" {
 # ECR
 module "ecr" {
   source          = "terraform-aws-modules/ecr/aws"
-  repository_name = "foododo-landing"
+  repository_name = "foododo"
 
   repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
   repository_lifecycle_policy = jsonencode({
     rules = [
       {
         rulePriority = 1,
-        description  = "Keep last 10 images",
+        description  = "Keep last 5 images",
         selection = {
           tagStatus     = "tagged",
           tagPrefixList = ["v"],
           countType     = "imageCountMoreThan",
-          countNumber   = 10
+          countNumber   = 5
 
         },
         action = {
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "AWS_REGION" {
+  description = "aws-region"
+  default     = "ap-southeast-1"
+  type        = string
+}
+
+variable "AWS_ACCOUNT_ID" {
+  description = "aws-account-id"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "aws-key"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "aws-secret"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
+variable "GITHUB_TOKEN" {
+  description = "github-token"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
+variable "env_vars" {
+  default = {
+    AWS_REGION     = "ap-southeast-1"
+    AWS_ACCOUNT_ID = "640940679593"
+    PROJECT        = "foododo-landing"
+  }
+}
