f23100b54f816040aa067b70f20771519f300023
Shao Wei <33794625+choyshaowei@users.noreply.github.com>
2023-06-16 02:18:53 +0800

    wip: add ecs

--- codepipeline.tf
+++ codepipeline.tf
@@ -1,6 +1,158 @@
+# ECS
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = var.ECSClusterName
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  configuration {
+    execute_command_configuration {
+      logging = "DEFAULT"
+    }
+  }
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT", aws_ecs_capacity_provider.ec2_capacity_provider.name]
+
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.ec2_capacity_provider.name
+    weight            = 1
+    base              = 0
+  }
+}
+
+resource "aws_launch_template" "ecs_launch_template" {
+  image_id      = data.aws_ssm_parameter.LatestECSOptimizedAMI.value
+  instance_type = "t4g.small"
+
+  iam_instance_profile {
+    arn = var.IamRoleInstanceProfile
+  }
+
+  user_data = base64encode(var.UserData)
+}
+
+resource "aws_autoscaling_group" "ecs_auto_scaling_group" {
+  launch_template {
+    id      = aws_launch_template.ecs_launch_template.id
+    version = "$Latest"
+  }
+  desired_capacity    = 1
+  min_size            = 1
+  max_size            = 1
+  vpc_zone_identifier = var.SubnetIds
+  tag {
+    key                 = "Name"
+    value               = "ECS Instance - ${var.ECSClusterName}"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_ecs_capacity_provider" "ec2_capacity_provider" {
+  name = "${var.ECSClusterName}CapacityProvider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.ecs_auto_scaling_group.arn
+
+    managed_scaling {
+      status                    = "ENABLED"
+      target_capacity           = 100
+      minimum_scaling_step_size = 1
+      maximum_scaling_step_size = 100
+    }
+
+    managed_termination_protection = "DISABLED"
+  }
+}
+
+# Security Group
+resource "aws_security_group" "your_security_group" {
+  name        = "your_security_group"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.VpcId
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# IAM Role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Effect = "Allow",
+      },
+    ]
+  })
+}
+
+# Task Definition
+resource "aws_ecs_task_definition" "task_def" {
+  family                   = "service_family"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "your_container_name"
+      image     = "${var.AWS_ACCOUNT_ID}.dkr.ecr.${var.AWS_REGION}.amazonaws.com/${var.PROJECT}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+# ECS Service
+resource "aws_ecs_service" "ecs_service" {
+  name            = "your_service_name"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_def.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.SubnetIds
+    assign_public_ip = true
+    security_groups  = [aws_security_group.your_security_group.id]
+  }
+}
+
+output "ECSCluster" {
+  description = "The created cluster."
+  value       = aws_ecs_cluster.ecs_cluster.arn
+}
+
+output "debug_SubnetIds" {
+  description = "Debug output for SubnetIds"
+  value       = var.SubnetIds
+}
+
+
 # CodePipeline
 resource "aws_s3_bucket" "foododo" {
-  bucket        = "foododo"
+  bucket        = "foododo-terraform"
   force_destroy = true
 }
 
@@ -21,7 +173,7 @@ resource "aws_codestarconnections_connection" "github" {
   provider_type = "GitHub"
 }
 resource "aws_codepipeline" "pipeline" {
-  name     = "foododo-pipeline"
+  name     = "foododo-terrafrom-pipeline"
   role_arn = aws_iam_role.pipeline.arn
 
   artifact_store {
@@ -74,7 +226,7 @@ resource "aws_codepipeline" "pipeline" {
       owner            = "AWS"
       provider         = "CodeBuild"
       input_artifacts  = ["source_output"]
-      output_artifacts = ["build_output"]
+      output_artifacts = ["build_output", "image_definitions"]
       version          = "1"
       configuration = {
         ProjectName = aws_codebuild_project.foododo.name
@@ -84,27 +236,27 @@ resource "aws_codepipeline" "pipeline" {
 
   stage {
     name = "Deploy"
-
     action {
       name            = "Deploy"
       category        = "Deploy"
       owner           = "AWS"
-      provider        = "S3"
-      input_artifacts = ["build_output"]
+      provider        = "ECS"
+      input_artifacts = ["image_definitions"]
       version         = "1"
-
       configuration = {
-        BucketName = aws_s3_bucket.foododo.bucket
-        Extract    = "true"
+        ClusterName = aws_ecs_cluster.ecs_cluster.name
+        ServiceName = aws_ecs_service.ecs_service.name
+        FileName    = "imagedefinitions.json"
       }
     }
   }
+
 }
 
 # Build and deploy to ECR as a container
 resource "aws_codebuild_project" "foododo" {
-  name          = "foododo"
-  description   = "foododo"
+  name          = "foododo-terraform"
+  description   = "foododo-terraform"
   service_role  = aws_iam_role.codebuild.arn
   build_timeout = "5"
 
@@ -269,7 +421,7 @@ resource "aws_iam_role_policy" "codebuild" {
 
 module "ecr" {
   source          = "terraform-aws-modules/ecr/aws"
-  repository_name = "foododo"
+  repository_name = "foododo-terraform"
 
 
   repository_read_write_access_arns = [aws_iam_role.ecr_role.arn]
@@ -296,5 +448,4 @@ module "ecr" {
     Environment = "dev"
   }
 }
-
 
--- variables.tf
+++ variables.tf
@@ -25,6 +25,13 @@ variable "AWS_SECRET_ACCESS_KEY" {
   default     = ""
 }
 
+variable "PROJECT" {
+  description = "project"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
 variable "GITHUB_TOKEN" {
   description = "github-token"
   type        = string
@@ -33,9 +40,47 @@ variable "GITHUB_TOKEN" {
 }
 
 variable "env_vars" {
+  type = map(string)
   default = {
     AWS_REGION     = "ap-southeast-1"
     AWS_ACCOUNT_ID = "640940679593"
-    PROJECT        = "foododo"
+    PROJECT        = "foododo-terraform"
   }
 }
+
+variable "ECSClusterName" {
+  description = "Specifies the ECS Cluster Name with which the resources would be associated"
+  type        = string
+  default     = "arm64ClusterPipeline"
+}
+
+variable "VpcId" {
+  description = "Optional - Specifies the ID of an existing VPC in which to launch your container instances. If you specify a VPC ID, you must specify a list of existing subnets in that VPC. If you do not specify a VPC ID, a new VPC is created with at least 1 subnet."
+  type        = string
+  default     = ""
+  validation {
+    condition     = can(regex("^(?:vpc-[0-9a-f]{8,17}|)$", var.VpcId))
+    error_message = "VPC Id must begin with 'vpc-' and have a valid uuid."
+  }
+}
+
+variable "SubnetIds" {
+  description = "Optional - Specifies the Comma separated list of existing VPC Subnet Ids where ECS instances will run"
+  type        = list(string)
+  default     = ["subnet-0d42c307a30ff2ade", "subnet-0fbc05a19239574dc"]
+}
+data "aws_ssm_parameter" "LatestECSOptimizedAMI" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2023/arm64/recommended/image_id"
+}
+variable "UserData" {
+  description = "User data required for Launch Template and Spot Fleet"
+  type        = string
+  default     = "#!/bin/bash \necho ECS_CLUSTER=arm64Cluster >> /etc/ecs/ecs.config;"
+}
+
+variable "IamRoleInstanceProfile" {
+  description = "Specifies the Name or the Amazon Resource Name (ARN) of the instance profile associated with the IAM role for the instance"
+  type        = string
+  default     = "arn:aws:iam::640940679593:instance-profile/ecsInstanceRole"
+}
+
